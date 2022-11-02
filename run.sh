ab90eb7c0e89f5feed63c2f7b221b8e6681d8030
Zeyan Li 李则言 <li_zeyan@icloud.com>
2022-11-02 16:09:49 +0800

    use venv interpreter

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd ${SCRIPT_DIR}
-python3 ${SCRIPT_DIR}/GSASummary.py
+./venv/bin/python3 ${SCRIPT_DIR}/GSASummary.py

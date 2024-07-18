52ea17a731dbb60a849de0390481a18e9829f575
Patrick Rathje <git@patrickrathje.de>
2024-07-18 14:16:19 +0200

    Allow parsing eml emails from directories

--- GSASummary.py
+++ GSASummary.py
@@ -3,6 +3,7 @@ import quopri
 import smtplib
 import sys
 import os
+import email
 from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta
@@ -42,13 +43,10 @@ latest_scholar_emails = []
 # ==============================================
 
 
-def is_email_content_latest(email: Emlx):
-    data_received = datetime.fromtimestamp(email.plist["date-received"])
-    return data_received >= DATETIME_THRESHOLD
 
 
 def is_path_latest(path: Path):
-    return path.stat(follow_symlinks=True).st_mtime > DATETIME_THRESHOLD.timestamp()
+    return path.stat().st_mtime > DATETIME_THRESHOLD.timestamp()
 
 
 def parse_scholar_url(url: str) -> str:
@@ -59,8 +57,7 @@ def parse_scholar_url(url: str) -> str:
         return url
 
 
-def is_scholar_alert(email: Emlx):
-    sender = email.headers["From"]
+def is_scholar_alert(sender):
     return "<scholaralerts-noreply@google.com>" in sender \
            or "<scholarcitations-noreply@google.com>" in sender \
            or "Google Scholar" in sender \
@@ -71,7 +68,7 @@ PAPER_ITEM = namedtuple(
     "PAPER_ITEM",
     [
         "title", "abstract", "venue_year", "authors",
-        "doi", "type", "url", 'reason',
+        "doi", "type", "url", 'reason', 'date'
     ]
 )
 
@@ -107,7 +104,7 @@ def google_scholar_search(title: str) -> Optional[dict]:
 
 @lru_cache(maxsize=None)
 def get_paper_detail_from_dblp(
-        *, title: str, scholar_abstract: str, scholar_author_venue: str, url: str, reason: str,
+        *, title: str, scholar_abstract: str, scholar_author_venue: str, url: str, reason: str, date,
 ) -> PAPER_ITEM:
     try:
         scholar_author = scholar_author_venue.split(" - ")[0]
@@ -159,6 +156,7 @@ def get_paper_detail_from_dblp(
                 type=pub_type,
                 url=url,
                 reason=reason,
+                date=date
             )
     except Exception as e:
         logger.error(f"Encounter exception when querying {title=}: {e}")
@@ -171,45 +169,91 @@ def get_paper_detail_from_dblp(
         authors=scholar_author,
         url=scholar_url,
         reason=reason,
+        date=date
     )
+
+def extract_from_emlx(email_path: Path):
+    email: Emlx = emlx.read(str(email_path), plist_only=True)
+
+    data_received = datetime.fromtimestamp(email.plist["date-received"])
+    if not data_received >= DATETIME_THRESHOLD:
+        return None
+
+    email: Emlx = emlx.read(str(email_path), plist_only=False)
+    if not is_scholar_alert(email.headers["From"]):
+        return None
+
+    return {
+        'Received': email.headers["Received"],
+        'From': email.headers["From"],
+        'Subject': email.headers.get('Subject', None),
+        'Date': email.headers.get('Date', None),
+        'html': getattr(email, "html"),
+    }
+
+
+def extract_from_eml(email_path: Path):
+    with open(str(email_path)) as email_file:
+        e = email.message_from_file(email_file)
+
+        if not is_scholar_alert(e["From"]):
+            return None
+
+        return {
+            'Received': e["Received"],
+            'From': e["From"],
+            'Subject': e["Subject"],
+            'Date': e["Date"],
+            'html': e.get_payload(),
+        }
+
+def parse_raw_mail(subject, date, html):
+    pq = PyQuery(quopri.decodestring(html))
+    titles = [_.text_content().encode("latin1").decode("utf-8") for _ in pq(".gse_alrt_title")]
+    abstracts = [
+        " ".join(_.text_content().replace("\n", " ").encode("latin1").decode("utf-8").split())
+        for _ in pq(".gse_alrt_sni")
+    ]
+    author_venues = [
+        " ".join(_.text_content().replace("\n", " ").encode("latin1").decode("utf-8").split())
+        for _ in pq(".gse_alrt_sni").prev()
+    ]
+    urls = [
+        parse_scholar_url(_.attrib["href"])
+        for _ in pq(".gse_alrt_title")
+    ]
+    __paper_titles = "\n\t\t\t\t".join(titles)
+    reason = pq('p')[-1].text_content().encode('latin1').decode('utf-8')
+
+    date = email.utils.parsedate_to_datetime(date).strftime('%Y-%m-%d')
+    logger.info(
+        f"Parsed Email Detail: \n"
+        f"\tSubject:\t{subject}\n"
+        f"\tDate:\t\t{date}\n"
+        f"\tReason:\t\t{reason}\n"
+        f"\tPapers:\t\t{__paper_titles}\n"
+    )
+    return titles, abstracts, author_venues, urls, reason, date
+
+
 
 
 def parse_email_from_path(email_path: Path) -> Dict[str, PAPER_ITEM]:
     try:
-        email: Emlx = emlx.read(str(email_path), plist_only=True)
-        if not is_email_content_latest(email):
+        if email_path.suffix == '.emlx':
+            mail_content = extract_from_emlx(email_path)
+        else:
+            mail_content = extract_from_eml(email_path)
+
+        if mail_content is None:
             return {}
-        email: Emlx = emlx.read(str(email_path), plist_only=False)
-        if not is_scholar_alert(email):
-            return {}
-        pq = PyQuery(quopri.decodestring(getattr(email, "html")))
-        titles = [_.text_content().encode("latin1").decode("utf-8") for _ in pq(".gse_alrt_title")]
-        abstracts = [
-            " ".join(_.text_content().replace("\n", " ").encode("latin1").decode("utf-8").split())
-            for _ in pq(".gse_alrt_sni")
-        ]
-        author_venues = [
-            " ".join(_.text_content().replace("\n", " ").encode("latin1").decode("utf-8").split())
-            for _ in pq(".gse_alrt_sni").prev()
-        ]
-        urls = [
-            parse_scholar_url(_.attrib["href"])
-            for _ in pq(".gse_alrt_title")
-        ]
+
         ret = {}
-        __paper_titles = "\n\t\t\t\t".join(titles)
-        reason = pq('p')[-1].text_content().encode('latin1').decode('utf-8')
-        logger.info(
-            f"Parsed Email Detail: \n"
-            f"\tSubject:\t{email.headers.get('Subject', 'NaN')}\n"
-            f"\tDate:\t\t{email.headers.get('Date', 'NaN')}\n"
-            f"\tReason:\t\t{reason}\n"
-            f"\tPapers:\t\t{__paper_titles}\n"
-        )
+        titles, abstracts, author_venues, urls, reason, date = parse_raw_mail(mail_content['Subject'], mail_content['Date'], mail_content['html'])
         for title, abstract, author_venue, url in zip(titles, abstracts, author_venues, urls):
             ret[title] = get_paper_detail_from_dblp(
                 title=title, scholar_abstract=abstract, scholar_author_venue=author_venue, url=url,
-                reason=reason,
+                reason=reason, date=date
             )
         return ret
     except Exception as e:
@@ -217,20 +261,53 @@ def parse_email_from_path(email_path: Path) -> Dict[str, PAPER_ITEM]:
         return {}
 
 
+def group_papers_by_end_date(papers):
+    return [
+            {
+                'title': END_DATE_STR,
+                'papers': list(sorted(papers.values(), key=lambda _: _["title"].lower()))
+             }
+    ]
+
+def group_papers_by_date(papers):
+
+    by_date = list(sorted(papers.values(), key=lambda _: (_["date"].lower(), _["title"].lower())))
+
+    last_date = ''
+
+    groups = []
+
+    for p in by_date:
+        if p['date'] != last_date:
+            groups.append({
+                'title': p['date'],
+                'papers': []
+            })
+            last_date = p['date']
+        groups[-1]['papers'].append(p)
+
+    return groups
+
 def render_report():
     env = Environment(loader=FileSystemLoader("./templates"))
     with open(f"output/{END_DATE_STR}.json", "r") as f:
         papers = json.load(f)
     template = env.get_template("report.html")
     with open(f"output/{END_DATE_STR}.html", "w+") as f:
+
+        groups = group_papers_by_date(papers)
+
         print(template.render(
-            papers=list(sorted(papers.values(), key=lambda _: _["title"].lower())),
+            groups=groups,
             start_datetime=DATETIME_THRESHOLD.strftime("%Y-%m-%d"),
             end_datetime=END_DATE_STR
         ), file=f)
 
 
 def send_email():
+    if "" in [TARGET_EMAIL, MAIL_USER]:
+        return
+
     msg = EmailMessage()
     msg['Subject'] = f'Google Scholar Alerts Summary on {END_DATE_STR}'
     msg["From"] = ME
@@ -252,7 +329,7 @@ def parse_papers_from_emails():
     with ThreadPoolExecutor() as executor:
         paper_dicts_of_emails = list(executor.map(
             parse_email_from_path,
-            filter(is_path_latest, data_root.glob("**/*.emlx"))
+            filter(is_path_latest, data_root.glob("**/*.eml*"))
         ))
     if len(paper_dicts_of_emails) == 0:
         logger.info("No recent alert found.")
@@ -272,7 +349,13 @@ def parse_papers_from_emails():
 
 
 def main():
-    if parse_papers_from_emails() == 0:
+    res = parse_papers_from_emails()
+
+    if res is None:
+        logger.info("No alerts found.")
+        return
+
+    if res == 0:
         logger.info("No recent paper found.")
         return
 
@@ -297,8 +380,22 @@ if __name__ == '__main__':
             compression="zip", level="INFO",
         )
 
+        # Create output directory
+        Path("output").mkdir(parents=True, exist_ok=True)
+
+        # Parse arguments
+        num_days = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+
+        if len(sys.argv) > 2:
+            data_root = Path(sys.argv[2]).expanduser()
+
         END_DATE_STR = datetime.now().strftime('%Y-%m-%d')
-        DATETIME_THRESHOLD = datetime.now() - timedelta(days=int(sys.argv[1]) if len(sys.argv) > 1 else 1)
+
+        if num_days > 0:
+            DATETIME_THRESHOLD = datetime.now() - timedelta(days=num_days)
+        else:
+            DATETIME_THRESHOLD = datetime.now() - timedelta(weeks=1000*12*4)
+
         logger.info(f"START {DATETIME_THRESHOLD} - {END_DATE_STR}")
         # set_proxy()
         main()
